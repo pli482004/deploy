97b5d0fb96b72e4380ad656a4ba40e3a7c89a9f8
Philip Li <saywhat@Philips-Air.attlocal.net>
2021-07-06 23:03:49 -0500

    dwdaw

--- deploy/settings.py
+++ deploy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['pipimac.herokuapp.com', '127.0.0.1', '0.0.0.0', 'www.pliweb.co
 # Application definition
 
 INSTALLED_APPS = [
+    'website',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
