c61e4ffad1e03cdf0f9e128c23f3e2653cdbab0a
Philip Li <saywhat@Philips-Air.attlocal.net>
2021-07-06 23:10:50 -0500

    dwdw

--- website/urls.py
+++ website/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('/about', views.about, name='about'),
     path('/select', views.select, name='select'),
-    path('/register', views.register, name='register')
+    path('/register', views.register, name='register'),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
 ]
--- website/views.py
+++ website/views.py
@@ -43,3 +43,28 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "website/register.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "auctions/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
