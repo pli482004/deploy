4bffba546ed5be390fb842be739369d59b339af9
Philip Li <saywhat@Philips-Air.attlocal.net>
2021-07-08 22:01:24 -0500

    dddd

--- website/views.py
+++ website/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from website.models import *
 from django.db import IntegrityError
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
+from django.templatetags.static import static
+import tensorflow as tf
+import json
+from PIL import Image
+from io import BytesIO
+from keras.preprocessing.image import img_to_array
+import numpy as np
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
@@ -69,3 +77,32 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
+
+@csrf_exempt
+def send(request):
+    global x
+    if request.method == "POST":
+        ImageData = json.loads(request.body.decode())['data']
+        import re
+        import base64
+
+        dataUrlPattern = re.compile("data:image/(png|jpeg);base64,(.*)$")
+        ImageData = dataUrlPattern.match(ImageData).group(2)
+
+        # If none or len 0, means illegal image data
+        if ImageData is None or len(ImageData) == 0:
+            print("failed image")
+            pass
+
+        im = Image.open(BytesIO(base64.b64decode(ImageData))).convert('LA')
+
+        im = im.resize((28, 28), Image.ANTIALIAS)
+        background = Image.new("RGB", im.size, (255, 255, 255))
+        background.paste(im, mask=im.split()[3])
+        test_image = np.array([img_to_array(background)])
+
+        model = tf.keras.models.load_model(static('website/model.h5'))
+
+        x = np.argmax(model.predict(test_image), axis=-1)
+
+    return JsonResponse({"hello": x}, status=200)
