169ecb5b04bbb848bb083cf24bcc0bb2f6de5362
Philip Li <saywhat@Philips-Air.attlocal.net>
2021-07-23 00:24:42 -0500

    dddd

--- website/urls.py
+++ website/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('/register', views.register, name='register'),
     path("/login", views.login_view, name="login"),
     path("/logout", views.logout_view, name="logout"),
-    path("/draw", views.draw, name="draw"),
+    # path("/draw", views.draw, name="draw"),
     path("/bookmark", views.bookmark, name="bookmark")
 ]
