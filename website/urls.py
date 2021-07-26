11234db91efb7902b2512aa0adce3b3575e864f6
Philip Li <saywhat@Philips-Air.attlocal.net>
2021-07-25 21:37:50 -0500

    ddwaw

--- website/urls.py
+++ website/urls.py
@@ -4,11 +4,11 @@ from website import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/about', views.about, name='about'),
-    path('/select', views.select, name='select'),
-    path('/register', views.register, name='register'),
-    path("/login", views.login_view, name="login"),
-    path("/logout", views.logout_view, name="logout"),
+    path('about', views.about, name='about'),
+    path('select', views.select, name='select'),
+    path('register', views.register, name='register'),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
     # path("/draw", views.draw, name="draw"),
-    path("/bookmark", views.bookmark, name="bookmark")
+    path("bookmark", views.bookmark, name="bookmark")
 ]
