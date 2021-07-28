4619742bccc4060e0155f143c433df7790a72fac
Philip Li <saywhat@Philips-Air.attlocal.net>
2021-07-28 01:03:24 -0500

    dd

--- website/views.py
+++ website/views.py
@@ -29,50 +29,50 @@ def select(request):
 
 
 def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
-
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "website/register.html", {
-                "message": "Passwords must match."
-            })
-
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "auctions/register.html", {
-                "message": "Username already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, "website/register.html")
+    # if request.method == "POST":
+    #     username = request.POST["username"]
+    #     email = request.POST["email"]
+    #
+    #     # Ensure password matches confirmation
+    #     password = request.POST["password"]
+    #     confirmation = request.POST["confirmation"]
+    #     if password != confirmation:
+    #         return render(request, "website/register.html", {
+    #             "message": "Passwords must match."
+    #         })
+    #
+    #     # Attempt to create new user
+    #     try:
+    #         user = User.objects.create_user(username, email, password)
+    #         user.save()
+    #     except IntegrityError:
+    #         return render(request, "auctions/register.html", {
+    #             "message": "Username already taken."
+    #         })
+    #     login(request, user)
+    #     return HttpResponseRedirect(reverse("index"))
+    # else:
+    return render(request, "website/register.html")
 
 
 def login_view(request):
-    if request.method == "POST":
-
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "website/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "website/login.html")
+    # if request.method == "POST":
+    #
+    #     # Attempt to sign user in
+    #     username = request.POST["username"]
+    #     password = request.POST["password"]
+    #     user = authenticate(request, username=username, password=password)
+    #
+    #     # Check if authentication successful
+    #     if user is not None:
+    #         login(request, user)
+    #         return HttpResponseRedirect(reverse("index"))
+    #     else:
+    #         return render(request, "website/login.html", {
+    #             "message": "Invalid username and/or password."
+    #         })
+    # else:
+    return render(request, "website/login.html")
 
 
 def logout_view(request):
